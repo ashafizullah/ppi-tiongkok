90333ce53537e88d176aabca63d287099f2d31e7
ashafizullah <adamsuchihafizullah@gmail.com>
2018-02-19 15:31:55 +0800

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,53 @@
+var app = new Framework7({
+  // App root element
+  root: '#app',
+  // App Name
+  name: 'PPI Tiongkok',
+  // App id
+  id: 'org.ppitiongkok',
+  // Enable swipe panel
+  panel: {
+    swipe: 'left',
+  },
+  // Add default routes
+  routes: [
+    {
+      path: '/home/',
+      url: './index.html',
+    },
+    {
+      path: '/sejarah/',
+      url: './pages/sejarah.html',
+    },
+    {
+      path: '/struktur/',
+      url: './pages/struktur.html',
+    },
+    {
+      path: '/duta/',
+      url: './pages/duta.html',
+    },
+    {
+      path: '/ketua/',
+      url: './pages/ketua.html',
+    },
+    {
+      path: '/chatbox/',
+      url: './pages/chatbox.html',
+    },
+    {
+      path: '/website-cabang/',
+      url: './pages/website_cabang.html',
+    },
+    {
+      path: '/gallery/',
+      url: './pages/gallery.html',
+    },
+    {
+      path: '/kritik-saran/',
+      url: './pages/kritik_saran.html',
+    },
+  ],
+});
+
+var mainView = app.views.create('.view-main');
